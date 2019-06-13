bee7f796ba88d0136cd934f8b2a8eb6b82c5b017
Arthur Martello <arthurmartello@outlook.com>
2019-06-13 15:51:39 +0200

    Résolution d'un bug d'affichage

--- src/gui/SimulationAvecUI.java
+++ src/gui/SimulationAvecUI.java
@@ -66,16 +66,19 @@ public class SimulationAvecUI extends GUIState {
 
                     if (((Humain) object).est(Statut.EN_ALERTE)) {
                         if (((Humain) object).est(Statut.EN_FEU))
-                            image = new ImageIcon("img/en_alerte_en_feu.png").getImage();
-                        else
-                            image = new ImageIcon("img/en_alerte.png").getImage();
-                    }
-                    else if (((Humain) object).est(Statut.PAR_TERRE)) {
+							image = new ImageIcon("img/en_alerte_en_feu.png").getImage();
+						else
+							image = new ImageIcon("img/en_alerte.png").getImage();
+
+                    } else if (((Humain) object).est(Statut.PAR_TERRE)) {
                         if (((Humain) object).est(Statut.EN_FEU))
                             image = new ImageIcon("img/par_terre_en_feu.png").getImage();
                         else
                             image = new ImageIcon("img/par_terre.png").getImage();
-                    }
+
+                    } else {
+						image = new ImageIcon("img/happy.png").getImage();
+					}
                 }
 
                 super.draw(object, graphics, info);
